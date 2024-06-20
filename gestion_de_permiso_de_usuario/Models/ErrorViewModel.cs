485a22b6ca7c004c334ad13358552ea64c251219
Carlos <carlos50957580@gmail.com>
2024-06-20 09:01:31 -0400

    proyecto creado

---
+++ gestion_de_permiso_de_usuario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gestion_de_permiso_de_usuario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
