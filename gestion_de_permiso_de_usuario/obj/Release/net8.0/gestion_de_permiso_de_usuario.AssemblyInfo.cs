8b63d9890befdfb19d30ef9ad376a2fa0670201a
Carlos <carlos50957580@gmail.com>
2024-07-08 09:44:10 -0400

    creacion de la vista creacion de permisos (por terminar)

--- gestion_de_permiso_de_usuario/obj/Release/net8.0/gestion_de_permiso_de_usuario.AssemblyInfo.cs
+++ gestion_de_permiso_de_usuario/obj/Release/net8.0/gestion_de_permiso_de_usuario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gestion_de_permiso_de_usuario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24901985c2ee7d2b5303498bb99d10054089e44d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e7a1d4cf73d6958248ed58537fcb5c7877dfd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("gestion_de_permiso_de_usuario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gestion_de_permiso_de_usuario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
