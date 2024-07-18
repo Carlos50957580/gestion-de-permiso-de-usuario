686bd059eb5542c79b68c35c1f4136b4efa8f271
Raidy <raidi16150905@gmail.com>
2024-07-18 11:57:44 -0400

    Registro completado

---
+++ gestion_de_permiso_de_usuario/Controllers/Login.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using gestion_de_permiso_de_usuario.Models;
+using Microsoft.Data.SqlClient;
+
+namespace gestion_de_permiso_de_usuario.Controllers
+{
+    public class Login : Controller
+    {
+        private string conexion = "Server=DESKTOP-LT3JSRI;Database=Pasantes;Integrated Security=True;TrustServerCertificate=True;";
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Registrar(Personas personas)
+        {
+            using SqlConnection con = new SqlConnection(conexion);
+            {
+                string agregarDatos = "INSERT INTO Personas(Nombre,Apellido,FechaNacimiento,Genero,Telefono,Correo) VALUES (@Nombre,@Apellido,@FechaNacimiento,@Genero,@Telefono,@Correo)";
+                
+                SqlCommand cmd = new SqlCommand(agregarDatos, con);
+                cmd.Parameters.AddWithValue("Nombre", personas.Nombre);
+                cmd.Parameters.AddWithValue("Apellido", personas.Apellido);
+                cmd.Parameters.AddWithValue("FechaNacimiento", personas.FechaNacimiento);
+                cmd.Parameters.AddWithValue("Genero", personas.Genero);
+                cmd.Parameters.AddWithValue("Telefono", personas.Telefono);
+                cmd.Parameters.AddWithValue("Correo", personas.Correo);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            return View();
+        }
+    }
+}
--- gestion_de_permiso_de_usuario/Models/User.cs
+++ gestion_de_permiso_de_usuario/Models/User.cs
@@ -15,4 +15,24 @@
 
         //Recuerda actualizar pendejo
     }
+
+
+    /*,[Nombre]
+      ,[Apellido]
+      ,[FechaNacimiento]
+      ,[Genero]
+      ,[Telefono]
+      ,[Correo]*/
+
+    public class Personas
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public string FechaNacimiento { get; set; }
+
+        public string Genero { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+    }
 }
