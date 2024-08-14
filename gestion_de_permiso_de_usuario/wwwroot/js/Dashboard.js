28451724b758c6f376a0c89d818956cdad190d0b
ReyRodriP <reynaldorodriguez802@gmail.com>
2024-08-14 11:17:18 -0400

    juntando los cambios

---
+++ gestion_de_permiso_de_usuario/wwwroot/js/Dashboard.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('#PermisoTable').DataTable({
+        "paging": true,
+        "searching": true,
+        "info": true,
+        "lengthChange": true,
+        "pageLength": 5,
+        "lengthMenu": [[5], [5]],
+        "language": {
+            "search": "Buscar:",
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        }
+    });
+
+});
