c76e861c1ff6fd66b0e6ca730cfb86fcf4493b9f
ReyRodriP <reynaldorodriguez802@gmail.com>
2024-08-23 10:47:27 -0400

    pantalla de carga agregada

--- gestion_de_permiso_de_usuario/obj/Debug/net8.0/gestion_de_permiso_de_usuario.AssemblyInfo.cs
+++ gestion_de_permiso_de_usuario/obj/Debug/net8.0/gestion_de_permiso_de_usuario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gestion_de_permiso_de_usuario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabb6128c062fe82075292d4bb22dcc39bf2edff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67091db270f586e7275bb0dfb733355c3aa84f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("gestion_de_permiso_de_usuario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gestion_de_permiso_de_usuario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
